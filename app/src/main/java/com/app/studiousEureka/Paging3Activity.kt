e2198578c80b9d390faf4d90e1768d33b907586f
rohitshukla0001 <jitendra@techalchemy.co>
2022-08-28 01:23:57 +0530

    initial commit

---
+++ app/src/main/java/com/app/studiousEureka/Paging3Activity.kt
@@ -0,0 +1,11 @@
+package com.app.studiousEureka
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Paging3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paging3)
+    }
+}
